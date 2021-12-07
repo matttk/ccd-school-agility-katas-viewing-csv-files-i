105247455f56894b3d9b0c9ee8fdfff519606d97
Matthew Kirschnick <matthew.kirschnick@node.energy>
2021-12-07 17:45:25 +0100

    Parse CSV file

--- csv-reader/csv-reader.js
+++ csv-reader/csv-reader.js
@@ -11,7 +11,41 @@ function readFile(filename) {
 }
 
 function parseCsvFile(filename) {
-  return readFile(filename);
+  const fileData = readFile(filename);
+  const data = parseFileData(fileData);
+
+  return data;
 }
 
-export { readFile, parseCsvFile };
+function parseFileData(fileDataAsString) {
+  const parsedData = {};
+
+  if (fileDataAsString.trim() === "") {
+    return parsedData;
+  }
+
+  const lines = splitStringByNewlines(fileDataAsString);
+  const header = parseLine(lines[0]);
+  const data = lines.slice(1).map(parseLine);
+
+  return {
+    header,
+    data,
+  };
+}
+
+function splitStringByNewlines(string) {
+  return string.split("\n");
+}
+
+function parseLine(line) {
+  return line.split(";");
+}
+
+export {
+  parseCsvFile,
+  parseFileData,
+  parseLine,
+  readFile,
+  splitStringByNewlines,
+};
--- csv-reader/csv-reader.test.js
+++ csv-reader/csv-reader.test.js
@@ -1,9 +1,65 @@
-import { readFile } from "./csv-reader.js";
+import {
+  parseFileData,
+  parseLine,
+  readFile,
+  splitStringByNewlines,
+} from "./csv-reader.js";
 
 describe("readFile", () => {
   it("returns a string", () => {
     const data = readFile("./persons.csv");
-
     expect(typeof data).toBe("string");
+  });
+});
+
+describe("splitStringByNewlines", () => {
+  it("returns array of length 3 for hello\\nworld\\n!", () => {
+    const array = splitStringByNewlines("hello\nworld\n!");
+    expect(array.length).toBe(3);
+  });
+});
+
+describe("parseLine", () => {
+  it("a;b;c returns array of length 3", () => {
+    const array = parseLine("a;b;c");
+    expect(array.length).toBe(3);
+  });
+});
+
+describe("parseFileData", () => {
+  describe("empty string", () => {
+    it("returns empty object", () => {
+      const data = parseFileData("");
+      expect(data).toStrictEqual({});
+    });
+  });
+
+  describe("some space", () => {
+    it("returns empty object", () => {
+      const data = parseFileData(" ");
+      expect(data).toStrictEqual({});
+    });
+  });
+
+  describe("Name;Age;City\\nPeter;42;New York\\nPaul;57;London", () => {
+    it("returns header with first line and data with second and third lines, each with array of size 3", () => {
+      const data = parseFileData(
+        "Name;Age;City\nPeter;42;New York\nPaul;57;London"
+      );
+
+      expect(Array.isArray(data.header)).toBe(true);
+      expect(data.header.length).toBe(3);
+      expect(Array.isArray(data.data)).toBe(true);
+      expect(data.data.length).toBe(2);
+      expect(data.data[0].length).toBe(3);
+      expect(data.data[1].length).toBe(3);
+      expect(data).toStrictEqual({
+        header: ["Name", "Age", "City"],
+        data: [
+          ["Peter", "42", "New York"],
+          ["Paul", "57", "London"],
+        ],
+      });
+    });
   });
 });
